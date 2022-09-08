39f6bdfce38c00ad153762d83bc98ba3212122c6
Maayanalt <maayanalt@gmail.com>
2022-09-08 15:10:47 +0300

    created the configuration for typeorm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'Aa123456',
+  database: 'customers',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
