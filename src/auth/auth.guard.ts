10f45171eec7fa74ec354a1732554c04f04dffa7
Maayan Altarac <maayanalt@gmail.com>
2022-10-05 22:05:20 +0300

    created auth guard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    if (req.session?.userID) {
+      return true;
+    }
+    return false;
+  }
+}
