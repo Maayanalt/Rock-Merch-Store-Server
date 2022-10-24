27738e9835c6e4a004845fb24c2207fc64b38797
Maayanalt <maayanalt@gmail.com>
2022-10-24 23:22:25 +0300

    added get one item by id in the controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,17 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
-  @Get()
-  findAll() {
-    return this.itemsService.getAll();
-  }
-
   @Get('/categories')
   getAllCategories() {
     return this.itemsService.getCategories();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.itemsService.getOneItem(+id);
+  }
+
+  @Get()
+  findAll() {
+    return this.itemsService.getAll();
   }
 }
