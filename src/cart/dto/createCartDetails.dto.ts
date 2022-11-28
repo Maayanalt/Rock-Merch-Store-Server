426deefae9204903a78899ccb5d8062b02ca38b3
Maayanalt <maayanalt@gmail.com>
2022-11-28 16:20:03 +0200

    created update and create cart details dto and removed cart details dto

--- src/cart/dto/createCartDetails.dto.ts
+++ src/cart/dto/createCartDetails.dto.ts
@@ -1,4 +1,4 @@
-export class CartDetailsDto {
+export class CreateCartDetailsDto {
   itemID: number;
   quantity: number;
   size: string;
---
+++ src/cart/dto/updateCartDetails.dto.ts
@@ -0,0 +1,6 @@
+export class UpdateCartDetailsDto {
+  cartDetailID: number;
+  itemID: number;
+  quantity: number;
+  size: string;
+}
