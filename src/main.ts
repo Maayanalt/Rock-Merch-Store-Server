cb997268053399f1aa8d135f9507271a71a08587
Maayanalt <maayanalt@gmail.com>
2023-11-22 22:34:28 +0200

    added trust proxy setting

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,21 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   const origin = configService.get('CORS_ORIGIN');
   const secret = configService.get('SECRET');
+
   app.enableCors({
     credentials: true,
     origin,
   });
 
+  app.set('trust proxy', 1);
   app.use(
     session({
       secret,
